eca4bb7ec5a0042f1364047e0f10e421e747ce11
Samuel Herrera <futbol.ingeniero@gmail.com>
2023-07-10 19:36:49 +0000

    Se agrego la notificacion cuando hay o no tareas registradas

--- src/js/component/todoCounter.jsx
+++ src/js/component/todoCounter.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
 export const TodoCounter = ({ totalTask }) => {
+  const sinTareas = () => {
+    const tareas = totalTask;
+    if (tareas == 0) {
+      return "Ninguna tarea registrada";
+    }
+
+    if (tareas == 1) {
+      return tareas + " Tarea por completar";
+    }
+
+    if (tareas > 1) {
+      return tareas + " Tareas por completar";
+    }
+  };
+
   return (
-    <div className="d-flex justify-content-start">
-      <span className=""> {totalTask} item left</span>
+    <div className="alert alert-secondary w-25 text-center" role="alert">
+      {sinTareas()}
     </div>
   );
 };
